351cccf4843f6a4b1ef699ceb41b04509b2c7802
qicaisheng <qicaisheng@qq.com>
2017-03-03 17:25:41 +0800

    Create consumer demo page.

--- consumer-demo/src/main/java/com/qicaisheng/pact/consumer/ConsumerDemoApplication.java
+++ consumer-demo/src/main/java/com/qicaisheng/pact/consumer/ConsumerDemoApplication.java
@@ -2,11 +2,19 @@ package com.qicaisheng.pact.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConsumerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String consumerDemoPage() {
+		return "This is consumer demo page.";
 	}
 }
