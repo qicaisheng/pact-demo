75ce9268a2e2ce431d621fd31b44271c52f5bb79
qicaisheng <qicaisheng@qq.com>
2017-03-09 15:36:07 +0800

    Create spring boot project as provider.

---
+++ provider-demo/src/main/java/com/qicaisheng/pact/provider/ProviderDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qicaisheng.pact.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderDemoApplication.class, args);
+	}
+}
