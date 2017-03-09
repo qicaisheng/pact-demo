83b59564e37d6c02c72b2dc62f1595e6f9f452cc
qicaisheng <qicaisheng@qq.com>
2017-03-09 15:46:46 +0800

    Create provider demo page.

--- provider-demo/src/main/java/com/qicaisheng/pact/provider/ProviderDemoApplication.java
+++ provider-demo/src/main/java/com/qicaisheng/pact/provider/ProviderDemoApplication.java
@@ -2,11 +2,19 @@ package com.qicaisheng.pact.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProviderDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String providerDemoPage() {
+		return "This is provider demo page.";
 	}
 }
